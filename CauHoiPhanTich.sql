73db2311b6f89791a07efa436093f58eda9e1bbf
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-09-17 00:28:13 +0700

    up

--- CauHoiPhanTich.sql
+++ CauHoiPhanTich.sql
@@ -24,5 +24,29 @@ JOIN bank_loan_accounts b
      ON a.customer_id = b.customer_id
 WHERE b.status <> 'closed'
 GROUP BY a.customer_id, a.full_name
-ORDER BY loan_count DESC, total_outstanding DESC;
+ORDER BY loan_count DESC, total_outstanding DESC
+
+                2. Dư nợ & dòng tiền
+-- Tổng dư nợ gốc của toàn bộ hệ thống tại ngày hiện tại
+SELECT SUM(loan_amount) AS total_outstanding_principal
+FROM bank_loan_accounts 
+WHERE status <> 'closed'
+-- Phân bổ dư nợ theo loan_type (personal, mortgage, business)
+SELECT 
+    loan_type,
+    SUM(loan_amount) AS total_outstanding_principal
+FROM bank_loan_accounts
+WHERE status <> 'closed'
+GROUP BY loan_type
+-- Top 10 khách hàng dư nợ lớn nhất
+SELECT TOP 10
+    a.customer_id,
+    a.full_name,
+    SUM(b.loan_amount) AS total_outstanding_principal
+FROM bank_customers a
+JOIN bank_loan_accounts b
+     ON a.customer_id = b.customer_id
+WHERE b.status <> 'closed'
+GROUP BY a.customer_id, a.full_name
+ORDER BY total_outstanding_principal DESC
 
