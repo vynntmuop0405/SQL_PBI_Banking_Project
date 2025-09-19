53b58e61175eada9dc2f8b9bcc91285fe4bd3f3b
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-09-19 23:46:13 +0700

    up

--- CauHoiPhanTich.sql
+++ CauHoiPhanTich.sql
@@ -50,3 +50,32 @@ WHERE b.status <> 'closed'
 GROUP BY a.customer_id, a.full_name
 ORDER BY total_outstanding_principal DESC
 
+            3. Lãi vay
+-- Tổng lãi thu được trong năm 2024
+SELECT SUM(amount) AS total_interest_2024
+FROM bank_transactions
+WHERE transaction_type = 'interest'
+  AND YEAR(transaction_date) = 2024
+-- Loan nào mang lại lãi suất cao nhất cho ngân hàng?
+SELECT TOP 1
+    loan_id,
+    interest_rate
+FROM bank_loan_accounts
+ORDER BY interest_rate DESC
+-- Trung bình lãi phải trả mỗi tháng của khách hàng vay personal loan
+SELECT 
+    AVG(monthly_interest) AS avg_monthly_interest_personal
+FROM (
+    SELECT 
+        a.customer_id,
+        a.full_name,
+        AVG(t.amount) AS monthly_interest
+    FROM bank_customers a
+    JOIN bank_loan_accounts b
+         ON a.customer_id = b.customer_id
+    JOIN bank_transactions c
+         ON b.loan_id = c.loan_id
+    WHERE b.loan_type = 'personal'
+      AND c.transaction_type = 'interest'
+    GROUP BY a.customer_id, c.full_name, YEAR(c.transaction_date), MONTH(c.transaction_date)
+) AS sub
